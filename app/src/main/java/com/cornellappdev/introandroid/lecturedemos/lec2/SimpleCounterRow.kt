ae6c3dfcb50f6f1f8a79fa8e6c0a9de013d7b7e5
Justin Guo <jjg283@cornell.edu>
2025-03-17 19:44:26 -0400

    Add simpler counter row example to lec2

---
+++ app/src/main/java/com/cornellappdev/introandroid/lecturedemos/lec2/SimpleCounterRow.kt
@@ -0,0 +1,53 @@
+package com.cornellappdev.introandroid.lecturedemos.lec2
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SimpleCounterRow() {
+    var count by remember { mutableStateOf(0) }
+
+    Row(
+        modifier = Modifier
+            .background(Color.White)
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "Count: $count",
+            fontSize = 24.sp
+        )
+
+        Button(
+            onClick = {
+                // TODO 1: Update count!
+            },
+        ) {
+            Text(text = "Add")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SimpleCounterRowPreview() {
+    SimpleCounterRow()
+}
